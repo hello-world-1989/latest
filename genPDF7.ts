693b42281d1091d025850d46bbcfdc5f781f54c8
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-04-21 09:32:49 +0000

    upload pdf

---
+++ genPDF7.ts
@@ -0,0 +1,46 @@
+import puppeteer from 'puppeteer';
+// import { saveAssetFile } from './githubAPIV2';
+
+// import * as fs from 'fs/promises';
+// import * as fs2 from 'fs';
+
+async function main() {
+  try {
+    const browser = await puppeteer.launch({ timeout: 60_000 });
+    const page = await browser.newPage();
+
+    try {
+      await page.goto(`https://end-gfw.com/tweet-page-7`);
+
+      const searchResultSelector = '.max-w-full';
+      await page.waitForSelector(searchResultSelector);
+
+      await page.pdf({
+        path: 'last.pdf',
+        width: 1440,
+        height: 1200,
+      });
+
+      // const yesterdayPDF = await fs.readFile('test.pdf');
+      // const fileSize = fs2.statSync('test.pdf').size;
+
+      // const base64String = yesterdayPDF.toString('base64');
+
+      // await saveAssetFile(
+      //   base64String,
+      //   'resource',
+      //   162922055,
+      //   'test.pdf',
+      //   fileSize
+      // );
+    } catch (err) {
+      console.log(`Error gen last 7 days pdf: `, err);
+    }
+
+    await browser.close();
+  } catch (err) {
+    console.log('Main error: ', err);
+  }
+}
+
+main();
